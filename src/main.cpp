e21408110d25b5d9feec30f2c1f466b7665dcd65
zSnails <erizojuan33@gmail.com>
2022-11-01 16:40:25 -0600

    Stash main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,38 @@
 #include <command_loader.hh>
 #include <context.hh>
+#include <cstdio>
 #include <filesystem>
 #include <iostream>
 #include <map>
 
 #include <string>
 
+#if defined(_WIN32)
+#define DL ".dll"
+#else
+#define DL ".so"
+#endif
+
 using std::string;
 
 int main() {
 
-  Context ctx;
-  std::map<string, Command> commands = std::map<string, Command>();
+  /* Context ctx; */
+  std::map<string, Command *> commands = std::map<string, Command *>();
 
-  for (const std::filesystem::directory_entry &dir :
+  for (const std::filesystem::directory_entry &entry :
        std::filesystem::directory_iterator("./")) {
 
-    Command *cmd = loadCommand(dir.path());
+    string fileName = entry.path();
 
-    commands.emplace(dir.path(), cmd);
+    int position = fileName.find_last_of(".");
+
+    string ft = fileName.substr(position);
+
+    if (ft.compare(DL) == 0) {
+      Command *cmd = loadCommand(entry.path());
+      commands.emplace(entry.path(), cmd);
+    }
   }
 
   return 0;
