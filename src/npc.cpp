21fdf4b0df1a94858ca01fca010f9463edaf486b
Johanx22x <johanrodsa2210@gmail.com>
2022-11-01 19:23:05 -0600

    Change NPC to Person in toString result

--- src/npc.cpp
+++ src/npc.cpp
@@ -3,6 +3,6 @@
 #include <npc.hh>
 
 std::string NPC::toString() {
-    std::string str = "NPC (" + std::to_string(id) + "):  " + name;
+    std::string str = "Person (" + std::to_string(id) + "):  " + name;
     return str;
 }
