9c09bdabfc46b0ef4f7339309004067c8a6566bc
johanx22x <johanrodsa2210@gmail.com>
2022-11-19 17:36:54 -0600

    Add modify-person command

--- src/commands/modify-person.cpp
+++ src/commands/modify-person.cpp
@@ -56,7 +56,61 @@ void command(Program *ctx) {
       break;
     }
     case 2: {
-        // TODO: Change person movement type
+      std::cout << "1 - Random\n";
+      std::cout << "2 - Adjacent\n";
+      std::cout << "3 - Through all\n";
+      std::cout << "4 - Direct\n";
+      std::cout << "Select movement type: ";
+      
+      int movementType = 0;
+      while (true) {
+          movementType = getInt();
+          if (movementType >= 1 && movementType <= 4) {
+              break;
+          }
+          std::cout << "Invalid movement type. Try again: ";
+      }
+      
+      MovementType type = static_cast<MovementType>(movementType - 1);
+
+      if (type == MovementType::DIRECT || type == MovementType::THROUGH_ALL) {
+        if (ctx->nodes->size < 2) {
+          std::cout << "There are no sufficient nodes to modify this person with this movement type.\nPlese create at least two nodes.\n";
+          return;
+        }
+
+        std::cout << "Available nodes:\n";
+        for (Node *curr = ctx->nodes->head; curr != nullptr; curr = curr->next) {
+          std::cout << curr->id << " - " << curr->name << "\n";
+        }
+
+        std::cout << "Enter the id of the node from: ";
+        int fromId = getInt();
+
+        Node *from = ctx->nodes->find(fromId);
+        if (from == nullptr) {
+          std::cout << "Invalid node id.\n";
+          return;
+        }
+
+        std::cout << "Enter the id of the node to: ";
+        int toId = getInt();
+
+        Node *to = ctx->nodes->find(toId);
+        if (to == nullptr) {
+          std::cout << "Invalid node id.\n";
+          return;
+        }
+
+        if (from == to) {
+          std::cout << "The nodes must be different.\n";
+          return;
+        }
+
+        person->from = from;
+        person->to = to;
+      }
+      person->mode = type;
       break;
     }
     case 3: {
