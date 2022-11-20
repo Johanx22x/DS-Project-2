087e3a64f1f165d7764ca83d156317f0a5c101a8
johanx22x <johanrodsa2210@gmail.com>
2022-11-20 13:07:28 -0600

    Add welcome message and credits

--- src/commands/exit.cpp
+++ src/commands/exit.cpp
@@ -3,7 +3,17 @@
 
 extern "C" {
 void command(Program *) {
-    std::cout << "See you later!\n";
+    std::cout << "See you later!\n\n";
+    std::cout <<
+"\u001b[0;32m    .---.     \n"
+"\u001b[0;32m   /     \\    \n"
+"\u001b[0;32m   \\.@-@./     \u001b[0mDeveloped by:\n"
+"\u001b[0;32m   /`\\_/`\\     \u001b[0m- @Johanx22x\n"
+"\u001b[0;32m  //  _  \\\\    \u001b[0m- @zSnails\n"
+"\u001b[0;32m | \\     )|_   \u001b[0m- @DavidArguedas\n"
+"\u001b[0;32m/`\\_`>  <_/ \\  \u001b[0m© 2022\n"
+"\u001b[0;32m\\__/'---'\\__/ \n";
+
     exit(0);
 }
 }
